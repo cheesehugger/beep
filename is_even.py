54922f3a0296b441b206e252ca6ddf00304b7226
cheesehugger <stephenzacarias2@gmail.com>
2022-04-22 23:00:38 -0700

    add the very advanced is_even.py as a new file

---
+++ is_even.py
@@ -0,0 +1,35 @@
+
+def is_even(num: int) -> bool:
+    if num == 1:
+        return False
+
+    elif num == 2:
+        return True
+
+    elif num == 3:
+        return False
+
+    elif num == 4:
+        return True
+
+    elif num == 5:
+        return False
+
+    elif num == 6:
+        return True
+
+    elif num == 7:
+        return False
+
+    elif num == 8:
+        return True
+
+    elif num == 9:
+        return False
+
+    elif num == 10:
+        return True
+
+
+if __name__ == "__main__":
+    print(is_even(1))
